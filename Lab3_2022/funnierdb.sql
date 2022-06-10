4be26d7efdbab586c53d0d9d6829c2d0a3e4db0a
davidquincoces17 <73016804+davidquincoces17@users.noreply.github.com>
2022-06-10 18:14:07 +0200

    fixed Database

--- Lab3_2022/funnierdb.sql
+++ Lab3_2022/funnierdb.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `funnierdb` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `funnierdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `funnierdb`;
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
@@ -18,41 +18,121 @@ USE `funnierdb`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `users`
+-- Table structure for table `follow`
 --
 
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `follow`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `username` varchar(10) NOT NULL,
-  `mail` varchar(50) NOT NULL,
-  `pwd` varchar(50) NOT NULL,
-  `nickname` varchar(30) NOT NULL,
-  `gender` varchar(6) NOT NULL,
-  `birth` varchar(10) NOT NULL,
-  PRIMARY KEY (`username`),
-  UNIQUE KEY `mail` (`mail`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+CREATE TABLE `follow` (
+  `userID` int NOT NULL,
+  `followedUser` int NOT NULL,
+  `timestamp` datetime NOT NULL,
+  PRIMARY KEY (`userID`,`followedUser`),
+  KEY `followedID_idx` (`followedUser`),
+  CONSTRAINT `followedID` FOREIGN KEY (`followedUser`) REFERENCES `user` (`id`),
+  CONSTRAINT `userID` FOREIGN KEY (`userID`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `follow`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('David','david@keonn.com','Hola123$','aaaaaaaaa','male','2000-02-08'),('David2','david@gmail.com','Hola123$','holadavid','male','2000-06-05'),('Davidaaa','oriol@gmail.com','Hola123$','aaaa','male','2000-05-05'),('DavidG','david@keonn','Hola123%','david','male','2000-02-07');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+LOCK TABLES `follow` WRITE;
+/*!40000 ALTER TABLE `follow` DISABLE KEYS */;
+INSERT INTO `follow` VALUES (1,4,'2022-06-03 07:03:25'),(2,3,'2022-06-03 02:11:53'),(4,1,'2022-06-03 07:01:23'),(4,2,'2022-06-03 21:14:22'),(4,3,'2022-06-03 03:21:41');
+/*!40000 ALTER TABLE `follow` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- Table structure for table `funny`
+--
 
--- Dump completed on 2022-06-10 16:04:57
+DROP TABLE IF EXISTS `funny`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `funny` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `parentID` int DEFAULT NULL,
+  `authorID` int NOT NULL,
+  `content` varchar(200) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `parentID_idx` (`parentID`),
+  KEY `authorID_idx` (`authorID`),
+  CONSTRAINT `authorID` FOREIGN KEY (`authorID`) REFERENCES `user` (`id`),
+  CONSTRAINT `parentID` FOREIGN KEY (`parentID`) REFERENCES `funny` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `funny`
+--
+
+LOCK TABLES `funny` WRITE;
+/*!40000 ALTER TABLE `funny` DISABLE KEYS */;
+INSERT INTO `funny` VALUES (1,NULL,2,'Hello funniers, here Marcos speaking!','2022-06-02 18:40:35'),(2,1,5,'Hello Marcos, here Maria speaking!','2022-06-02 19:36:19'),(3,NULL,3,'Hello everyone, here Xavi talking!','2022-06-02 21:46:09');
+/*!40000 ALTER TABLE `funny` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `funstate`
+--
+
+DROP TABLE IF EXISTS `funstate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `funstate` (
+  `userID` int NOT NULL,
+  `funnyID` int NOT NULL,
+  `state` tinyint NOT NULL,
+  PRIMARY KEY (`userID`,`funnyID`),
+  KEY `funnyID_idx` (`funnyID`),
+  CONSTRAINT `FSfunnyID` FOREIGN KEY (`funnyID`) REFERENCES `funny` (`id`),
+  CONSTRAINT `FSuserID` FOREIGN KEY (`userID`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `funstate`
+--
+
+LOCK TABLES `funstate` WRITE;
+/*!40000 ALTER TABLE `funstate` DISABLE KEYS */;
+INSERT INTO `funstate` VALUES (3,2,1);
+/*!40000 ALTER TABLE `funstate` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nickname` varchar(30) NOT NULL,
+  `gender` int NOT NULL,
+  `birth` date NOT NULL,
+  `profilePhoto` varchar(200) NOT NULL,
+  `isAdmin` tinyint NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'david','david.grao01@estudiant.upf.edu','Hola123%','elgato',0,'2000-02-07','images/image1.png',1),(2,'marcos','marcosariel.chindemi01@estudiant.upf.edu','Hola123%','elchindi',0,'1997-12-23','images/image2.png',1),(3,'xavi','xavi.gallardo01@estudiant.upf.edu','Hola123%','xvgg',0,'1999-11-22','images/image3.png',1),(4,'dave','david.quincoces01@estudiant.upf.edu','Hola123%','davee',0,'2000-12-25','images/image4.png',1),(5,'maria','maria.montserrat01@estudiant.upf.edu','Chau123%','marimontsi',1,'2000-01-01','images/image5.png',0);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
