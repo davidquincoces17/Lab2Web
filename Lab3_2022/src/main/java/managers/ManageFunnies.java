99b0771af67fa544ef2a920198f4c06c4bd7cdf0
Xavi <xagallardogallardo1@gmail.com>
2022-06-17 20:01:25 +0200

    La timeline se ve bastante fina (funnies propis i dels followeds)

--- Lab3_2022/src/main/java/managers/ManageFunnies.java
+++ Lab3_2022/src/main/java/managers/ManageFunnies.java
@@ -96,7 +96,7 @@ public class ManageFunnies {
 	
 	public List<Funny> getFollowedFunnies(Integer userID,Integer start, Integer end) {
 		
-		String query = "SELECT funny.id,funny.parentID,funny.authorID,funny.timestamp,funny.content,user.username FROM funny INNER JOIN user ON funny.authorID = user.id where funny.authorID IN (SELECT followedUser FROM follow WHERE userID = ? UNION SELECT ?) ORDER BY funny.timestamp DESC LIMIT ?,? ;";
+		String query = "SELECT funny.id,funny.parentID,funny.authorID,funny.timestamp,funny.content,user.username, user.nickname FROM funny INNER JOIN user ON funny.authorID = user.id where funny.authorID IN (SELECT followedUser FROM follow WHERE userID = ? UNION SELECT ?) ORDER BY funny.timestamp DESC LIMIT ?,? ;";
 		 PreparedStatement statement = null;
 		 List<Funny> l = new ArrayList<Funny>();
 		 try {
@@ -113,6 +113,8 @@ public class ManageFunnies {
 				 funny.setAuthorId(rs.getInt("authorID"));
 				 funny.setTimestamp(rs.getTimestamp("timestamp"));
 				 funny.setContent(rs.getString("content"));
+				 funny.setAuthorNickname(rs.getString("nickname"));
+				 funny.setAuthorUsername(rs.getString("username"));
 				 l.add(funny);
 			 }
 			 rs.close();
--- Lab3_2022/src/main/webapp/ViewOwnTimeline.jsp
+++ Lab3_2022/src/main/webapp/ViewOwnTimeline.jsp
@@ -11,7 +11,7 @@
 </script>
 
 <div class="w3-container w3-card w3-round w3-white w3-section">
-	<h6 class="w3-opacity"> Hello ${user.nickname}, would you like to share some Funny? </h6>
+	<h6 class="w3-opacity"> Hello ${user.username}, would you like to share some Funny? </h6>
 	<p id="tweetContent" contenteditable="true" class="w3-border w3-padding"> </p>
 	<button id="addTweet" type="button" class="w3-button w3-theme w3-section"><i class="fa fa-pencil"></i> &nbsp;Post</button> 
 </div>
