870f9cc3d2417ff711d3e4f6f8336211d11ca941
davidgrao2000 <david.grao01@estudiant.upf.edu>
2022-06-24 16:14:29 +0200

    New file delFunny

---
+++ Lab3_2022/src/main/java/controllers/DelFunny.java
@@ -0,0 +1,63 @@
+package controllers;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import managers.ManageFunnies;
+import models.Funny;
+import models.User;
+
+/**
+ * Servlet implementation class DelTweet
+ */
+@WebServlet("/DelTweet")
+public class DelFunny extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DelFunny() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		Funny funny = new Funny();
+		ManageFunnies funnyManager = new ManageFunnies();
+		HttpSession session = request.getSession(false);
+		User user = (User) session.getAttribute("user");
+
+		try {
+			if (session != null || user != null) {
+				BeanUtils.populate(funny, request.getParameterMap());
+				funnyManager.deleteFunny(funny.getId());
+				funnyManager.finalize();
+			}
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- Lab3_2022/src/main/webapp/ViewUserInfo.jsp
+++ Lab3_2022/src/main/webapp/ViewUserInfo.jsp
@@ -6,10 +6,10 @@
   <div > <img src="${user.profilePhoto}" class="w3-circle" style="height:106px;width:106px;margin-right:10px" alt="Avatar"></div>
   <div style="margin-top:20px;display:block">
   	<div style="font-size:20px">
-  		<b>  &nbsp &nbsp ${user.nickname}</b>
+  		<b> &nbsp &nbsp ${user.nickname}</b>
   	</div>
   	<div>
-  		<span class="w3-opacity">  &nbsp &nbsp @${user.username} </span>
+  		<span class="w3-opacity"> &nbsp &nbsp @${user.username} </span>
   	</div><br>
   	<div style=padding-left:20px>
   		<button type="button" style="height:25px;width:55px;font-size:12px;padding-top:0px;padding-bottom:0px;padding-left:0px;padding-right:0px" class="editUser w3-button w3-white w3-border  w3-round-large"><i class="fa fa-user"></i> &nbsp;Edit</button>
